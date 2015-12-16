ed0ca385f48a36062d3b64c88a49719f3831888b
Sander Vanheule <Sander.Vanheule@UGent.be>
2015-12-16 14:04:34 +0100

    Add support for toggling display test mode.

--- render.py
+++ render.py
@@ -56,6 +56,9 @@ class DisplayCom:
 #      time.sleep(1./100)
     self.port.write(data)
 
+  def toggle_test_mode(self):
+    self.port.write(b"\x42")
+
   def interrupt_handler(self, signal, frame):
     self.render_frame = False
     self.flush_buffer()
