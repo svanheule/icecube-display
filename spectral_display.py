b9ba2136d6a14a74d28300b83d31456dac63813b
Sander Vanheule <sander.vanheule@ugent.be>
2015-12-21 15:15:03 +0100

    Number of small fixes to generate correct display data.

--- spectral_display.py
+++ spectral_display.py
@@ -19,7 +19,7 @@ Gst.init(None)
 # Setup display specifics
 AUDIO_RATE = 44100
 BANDS = 10
-FPS = 5
+FPS = 25
 MIN_AMPLITUDE = -60
 
 PIXEL_COORDINATES = [
@@ -35,7 +35,7 @@ PIXEL_COORDINATES = [
   , [5,9], [6,9], [7,9], [8,9]
 ]
 LED_COUNT = len(PIXEL_COORDINATES)
-MAX_BRIGHTNESS = 2e4
+MAX_BRIGHTNESS = 2**2
 
 # Determine mapping of pixels to station
 ROW_START = [1, 7, 14, 22, 31, 41, 51, 60, 68, 75, 79]
@@ -75,28 +75,29 @@ def led_data(rgb):
   return [int(round(brightness*MAX_BRIGHTNESS)), rgb[0], rgb[1], rgb[2]]
 
 def led_position(band, amplitude, origin, column_height, direction):
-  position = numpy.round(abs((amplitude-MIN_AMPLITUDE)/MIN_AMPLITUDE*(column_height-1)))
+  position = numpy.ceil(abs((amplitude-MIN_AMPLITUDE)/MIN_AMPLITUDE*(column_height-1)))
   offset = [direction[0]*position+band, direction[1]*position]
   return numpy.add(origin, offset)
 
 def set_colour_pos(data, band, amplitude, origin, lengths, direction):
-  normalisation = 3*MIN_AMPLITUDE # 120/360 = 1/3
-  hue = abs(amplitude/normalisation)
-  saturation = 1.
-  value = 0.5
-  colour = colorsys.hsv_to_rgb(hue, saturation, value)
-
   col_led_count = lengths[band]
   position = led_position(band, amplitude, origin, col_led_count, direction)
   pixel = lookup_pixel(position)
-  data[pixel] = led_data(colour)
-  # TODO fill below top pixel
+
+  normalisation = 3*MIN_AMPLITUDE # 120/360 = 1/3
+  hue = abs(amplitude/normalisation)
+  saturation = 1.
+  value = 1.
+  colour = colorsys.hsv_to_rgb(hue, saturation, value)
+  data[4*pixel:4*(pixel+1)] = led_data(colour)
+
+  # Fill below top pixel
   top = int(direction[1]*(position[1] - origin[1]))
   for row in range(top):
     position = numpy.add(origin, [direction[0]*row+band, direction[1]*row])
     pixel = lookup_pixel(position)
     hue = (col_led_count-row)/(3*col_led_count)
-    data[pixel] = led_data(colorsys.hsv_to_rgb(hue, saturation, value))
+    data[4*pixel:4*(pixel+1)] = led_data(colorsys.hsv_to_rgb(hue, saturation, value))
 
 
 def render_spectrum(left, right):
@@ -108,7 +109,7 @@ def render_spectrum(left, right):
   RIGHT_DIRECTION = [0,-1]
   RIGHT_LENGTHS = [5, 5, 5, 5, 5, 5, 4, 3, 2, 1]
 
-  data = numpy.zeros((LED_COUNT, 4))
+  data = bytearray(LED_COUNT*4)
   # Render all the things!
   # Amplitude will range from MIN_AMPLITUDE to 0 dB, so divide this in led-count ranges per line
   # Use the hue range from 120 (green) to 0 deg (red).
@@ -118,7 +119,7 @@ def render_spectrum(left, right):
   for band, amplitude in enumerate(right):
     set_colour_pos(data, band, amplitude, RIGHT_ORIGIN, RIGHT_LENGTHS, RIGHT_DIRECTION)
 
-  return data.flatten()
+  return data
 
 
 def print_icetop(data):
@@ -172,7 +173,7 @@ def message_handler(bus, message):
     magnitudes = json.loads(s[s.find(sep)+len(sep):].translate(bytes.maketrans(b'<>;',b'[] ')))
     data = render_spectrum(magnitudes[0], magnitudes[1])
 #    print_icetop(data)
-    disp.send_frame(frame)
+    disp.send_frame(data)
 #  except KeyError as e:
 #    key = int(str(e))
 #    print("Key error: {},{}".format(key>>4, key&0xf))
@@ -189,8 +190,16 @@ if __name__ == "__main__":
 #  src = Gst.ElementFactory.make("pulsesrc", None)
 #  src.set_property("device", "alsa_output.pci-0000_00_1b.0.analog-stereo.monitor")
   src = Gst.ElementFactory.make("filesrc", "input")
-  src.set_property("location", "/home/sander/Muziek/Chromeo/Business Casual/02  - I'm Not Contagious.mp3")
+  if len(sys.argv) > 1:
+    location = sys.argv[1]
+  else:
+    location = "/home/sander/Muziek/Chromeo/Business Casual/02  - I'm Not Contagious.mp3"
+#    location = "/home/sander/Muziek/Metronomy/The English Riviera/04 The Look.mp3"
+  print("Playing {}".format(location))
+
+  src.set_property("location", location)
   decode = Gst.ElementFactory.make("mad", "decode")
+  convert = Gst.ElementFactory.make("audioconvert", "convert")
 
   # Raw audio filter
   caps_string = "audio/x-raw, format=(string)S32LE, rate=(int){}, channels=2, layout=(string)interleaved".format(AUDIO_RATE)
@@ -211,12 +220,14 @@ if __name__ == "__main__":
   pipeline = Gst.Pipeline.new("icetop-display-vu")
   pipeline.add(src)
   pipeline.add(decode)
+  pipeline.add(convert)
   pipeline.add(audio_filter)
   pipeline.add(spectrum)
   pipeline.add(sink)
 
   src.link(decode)
-  decode.link(audio_filter)
+  decode.link(convert)
+  convert.link(audio_filter)
   audio_filter.link(spectrum)
   spectrum.link(sink)
 
