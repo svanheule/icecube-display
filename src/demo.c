e9f9a2d8659dda5841e718274c50df8913d99183
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-01-13 10:51:07 +0100

    Test loading event pulses from PROGMEM.

--- src/demo.c
+++ src/demo.c
@@ -1,39 +1,107 @@
 #include "demo.h"
+#include <avr/pgmspace.h>
 
+// Create forward linked list stored in PROGMEM
+#define LED_CYAN {0x03, 0, 255, 255}
+#define LED_MAGENTA {0x03, 255, 0, 255}
+#define LED_YELLOW {0x03, 255, 255, 0}
+
+static const struct pulse_t pulses_0[] PROGMEM = {
+    {0, 0, LED_CYAN}
+  , {25, 12, LED_MAGENTA}
+  , {50, 13, LED_YELLOW}
+};
+static const struct pulse_t pulses_1[] PROGMEM = {
+    {0, 74, LED_CYAN}
+  , {25, 73, LED_MAGENTA}
+  , {50, 59, LED_YELLOW}
+};
+
+struct item_t {
+  struct event_t event;
+  struct item_t* next_item;
+};
+
+static struct item_t event_0, event_1;
+static struct item_t event_0 = {{3, pulses_0}, &event_1};
+static struct item_t event_1 = {{3, pulses_1}, 0};
+
+// Module global variables
+static const struct item_t* current_item;
 static const struct pulse_t* current_pulse;
 static const struct pulse_t* pulses_end;
-static uint8_t frame_number;
+
+static uint16_t frame_number;
 static uint8_t led_remaining_on[LED_COUNT];
+static uint16_t last_frame_number;
 
-static void render_frame(frame_t* buffer) {
+static void load_event(const struct item_t* item) {
+  frame_number = 0;
+  last_frame_number = 0;
 
-  // Decrement on-times and turn of timed out LEDs
-  uint8_t i;
-  for (i = 0; i < LED_COUNT; ++i) {
-    if (led_remaining_on[i] > 0) {
-      // Decrease remaining on-time
-      --led_remaining_on[i];
-      // If no more time remains, turn the LED off
-      if (!led_remaining_on[i]) {
-        (*buffer)[i] = (struct led_t) {
-            .brightness = 0
-          , .red = 0
-          , .green = 0
-          , .blue = 0
-        };
+  current_pulse = item->event.pulses;
+  pulses_end = current_pulse + item->event.length;
+}
+
+void init_demo() {
+  current_item = &event_0;
+  load_event(&event_0);
+  int i = LED_COUNT-1;
+  do {
+    led_remaining_on[i] = 0;
+  } while(i--);
+}
+
+uint8_t demo_finished() {
+  return current_item ? 0 : 1;
+}
+
+void render_demo(frame_t* buffer) {
+  if (frame_number == 0) {
+    clear_frame(buffer);
+  }
+
+  if (current_item) {
+    // Decrement on-times and turn off timed out LEDs
+    uint8_t i;
+    for (i = 0; i < LED_COUNT; ++i) {
+      if (led_remaining_on[i] > 0) {
+        // Decrease remaining on-time
+        --led_remaining_on[i];
+        if (led_remaining_on[i] == 0) {
+          (*buffer)[i] = (struct led_t) {0, 0, 0, 0};
+        }
+      }
+    }
+
+    // Set frame_number to its next value
+    ++frame_number;
+
+    // Read newly fired pulses
+    while (current_pulse != pulses_end && pgm_read_word(&(current_pulse->time)) < frame_number) {
+      // Load pulse from PROGMEM
+      struct pulse_t pulse;
+      memcpy_P(&pulse, current_pulse, sizeof(struct pulse_t));
+      // Set LED to its given colour and advance pointer
+      (*buffer)[pulse.led_index] = pulse.led;
+      led_remaining_on[pulse.led_index] = PULSE_DURATION;
+      last_frame_number = frame_number + PULSE_DURATION;
+      ++current_pulse;
+      if (current_pulse == pulses_end) {
+        last_frame_number += CLEAR_DURATION;
+      }
+    }
+
+    // Determination of event display ending
+    if (frame_number == last_frame_number && current_pulse == pulses_end) {
+      // Proceed to next list item
+      current_item = current_item->next_item;
+      if (current_item) {
+        load_event(current_item);
       }
     }
   }
-
-  // Set frame_number to its next value
-  ++frame_number;
-
-  // Read newly fired pulses
-  while (current_pulse != pulses_end && current_pulse->time < frame_number) {
-    (*buffer)[current_pulse->led_index] = current_pulse->led;
-    ++current_pulse;
+  else {
+    clear_frame(buffer);
   }
-
-  // TODO Determination of event display ending
-
 }
--- src/demo.h
+++ src/demo.h
@@ -5,6 +5,7 @@
 #include "frame_buffer.h"
 
 #define PULSE_DURATION 33 // In number of frames (25 fps), must be smaller than 128 (int8_t)
+#define CLEAR_DURATION 50
 
 struct pulse_t {
   uint16_t time; //< Turn-on time of led
