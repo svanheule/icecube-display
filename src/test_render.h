541aed075b132824248ccace5b55b1b563a30b09
Sander Vanheule <sander.vanheule@ugent.be>
2016-02-15 22:00:37 +0100

    Convert test rendering header to new style.

--- src/test_render.h
+++ src/test_render.h
@@ -4,9 +4,9 @@
 #include "frame_buffer.h"
 
 /// Render a number of pixels bouncing back and forth between the start and end of the buffer
-void render_snake(frame_t* buffer);
+const struct renderer_t* get_snake_renderer();
 
 /// Render consecutive concentric rings around pixel (5,4), i.e. station 36
-void render_ring(frame_t* buffer);
+const struct renderer_t* get_ring_renderer();
 
 #endif
