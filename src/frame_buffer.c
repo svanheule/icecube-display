cb75986c9358ccafa3fc93e798a4c03dfc1b61f6
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-02-16 15:25:37 +0100

    Fix wrong argument name.

--- src/frame_buffer.c
+++ src/frame_buffer.c
@@ -1,7 +1,7 @@
 #include "frame_buffer.h"
 
 void clear_frame(struct frame_buffer_t* frame_ptr) {
-  if (frame) {
+  if (frame_ptr) {
     struct led_t* write_ptr = frame_ptr->buffer;
     struct led_t* frame_end = write_ptr + LED_COUNT;
 
