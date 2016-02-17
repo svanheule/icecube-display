170973460faaebf82917f679f62daccddf3f6f57
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-02-17 15:55:59 +0100

    Add initial support for USB_ACT LED driving.

--- src/remote_usb.c
+++ src/remote_usb.c
@@ -1,7 +1,31 @@
 #include "remote.h"
+#include <stdbool.h>
+#include <avr/io.h>
 
-void init_remote() {}
+static void init_led();
+static void set_led_state(bool on);
+// TODO Timer3 ISR for blinking LED
+
+void init_remote() {
+  init_led();
+}
 
 bool is_remote_connected() {
   return false;
+}
+
+
+// USB_ACT LED control
+static void init_led() {
+  DDRB = _BV(DDB0);
+  set_led_state(true);
+}
+
+static void set_led_state(bool on) {
+  if (on) {
+    PORTB &= ~_BV(PB0);
+  }
+  else {
+    PORTB |= _BV(PB0);
+  }
 }
