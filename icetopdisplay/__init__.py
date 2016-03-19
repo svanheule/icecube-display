a1ed5caff7b118409552b8fe0d664d94c97f86be
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-03-19 15:48:53 +0100

    Add support for USB communication in python.

--- icetopdisplay/__init__.py
+++ icetopdisplay/__init__.py
@@ -1,3 +1,3 @@
-from .com import DisplayCom
+from .com import DisplayCom, DisplayComUart, DisplayComUsb
 from .textdisplay import TextDisplay
 from .led_format import FormatAPA102, FormatAPA102Comp
--- icetopdisplay/com.py
+++ icetopdisplay/com.py
@@ -5,11 +5,8 @@ import threading
 
 from .geometry import LED_COUNT
 
-
 class DisplayCom:
-
-  def __init__(self, device="/dev/ttyACM0"):
-    self.port = serial.Serial(device, 115200, timeout=0.1)
+  def __init__(self):
     self._connect()
     self._lock = threading.Lock()
 
@@ -18,6 +15,28 @@ class DisplayCom:
 
   def release(self):
     self._lock.release()
+
+  def _connect(self, waiting_time):
+    raise NotImplementedError
+
+  def _send_frame(self, frame):
+    raise NotImplementedError
+
+  # Send a frame buffer to the display.
+  # Use LedFormat to convert 3-tuples of floats to the correct buffer format.
+  def send_frame(self, data):
+    data = numpy.array(data, dtype=numpy.uint8).reshape(LED_COUNT*4)
+    frame = bytearray(data)
+    self._send_frame(frame)
+
+  def flush_buffer(self):
+    self._send_frame(b"\x00"*(LED_COUNT*4))
+
+
+class DisplayComUart(DisplayCom):
+  def __init__(self, device="/dev/ttyACM0"):
+    self.port = serial.Serial(device, 115200, timeout=0.1)
+    super().__init__()
 
   def _connect(self, waiting_time=2):
     # Ask for ID (command "I") and fail on timeout or incompatibility
@@ -40,19 +59,47 @@ class DisplayCom:
       self.port.write(b"\x41")
       self.port.write(frame)
 
-  def flush_buffer(self):
-    self._send_frame(b"\x00"*(LED_COUNT*4))
-
-  # Send a frame buffer to the display.
-  # Use LedFormat to convert 3-tuples of floats to the correct buffer format.
-  def send_frame(self, data):
-    data = numpy.array(data, dtype=numpy.uint8).reshape(LED_COUNT*4)
-    frame = bytearray(data)
-    self._send_frame(frame)
-
   def toggle_snake_test_mode(self):
     self.port.write(b"S")
 
   def toggle_ring_test_mode(self):
     self.port.write(b"R")
+
+
+# USB communication support
+import usb1
+
+class DisplayComUsb(DisplayCom):
+  def __init__(self):
+    self.usb_context = usb1.USBContext()
+    self._handle = None
+    self._device = None
+    super().__init__()
+
+  def acquire(self):
+    super().acquire()
+    self._handle = self._device.open()
+
+  def release(self):
+    self._handle.close()
+    self._handle = None
+    super().release()
+
+  def _connect(self, waiting_time=0):
+    devices = self.usb_context.getDeviceList(skip_on_error=True)
+    self._device = None
+    # Find IceCube device
+    for dev in devices:
+      if dev.getVendorID() == 0x1CE3:
+        self._device = dev
+
+  def _send_frame(self, frame):
+    if self._handle:
+      self._handle.controlWrite(
+          usb1.ENDPOINT_OUT | usb1.TYPE_VENDOR | usb1.RECIPIENT_INTERFACE
+        , 1
+        , 0
+        , 0
+        , bytes(frame)
+      )
 
