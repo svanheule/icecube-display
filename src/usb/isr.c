0b143b439eff9ea60b35841cdaf80d40ed97f2bd
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-04-04 11:23:24 +0200

    Change all UsbSetupPacket_t pointers to control_transfer_t pointers.

--- src/usb/isr.c
+++ src/usb/isr.c
@@ -118,18 +118,16 @@ enum control_stage_t {
 
 struct control_transfer_t {
   enum control_stage_t stage;
-  struct UsbSetupPacket_t req;
+  const struct UsbSetupPacket_t* req;
   void* data_in;
   uint16_t data_in_length;
   uint16_t data_in_done;
   void (*callback_data_out)(struct control_transfer_t* transfer);
-  void (*callback_handshake)(const struct UsbSetupPacket_t* req);
+  void (*callback_handshake)(struct control_transfer_t* transfer);
 };
 
-static struct control_transfer_t control_transfer;
-
-static void callback_set_address(const struct UsbSetupPacket_t* req);
-static void callback_set_configuration(const struct UsbSetupPacket_t* req);
+static void callback_set_address(struct control_transfer_t* transfer);
+static void callback_set_configuration(struct control_transfer_t* transfer);
 
 static void* init_data_in(struct control_transfer_t* transfer, size_t length) {
   transfer->data_in = malloc(length);
@@ -149,47 +147,47 @@ static void cancel_control_transfer(struct control_transfer_t* transfer) {
   }
 }
 
-static void process_standard_request(const struct UsbSetupPacket_t* req) {
-  switch (req->bRequest) {
+static void process_standard_request(struct control_transfer_t* transfer) {
+  switch (transfer->req->bRequest) {
     case GET_STATUS:
       // default state fail; address state fail if EP!=0; config state fail if non-existant
       // Device: self-powered, configured for remote wake-up
       // Interface: zeros
       // Endpoint: halted (interrupt and bulk required, other optional)
-      if (init_data_in(&control_transfer, 2)) {
-        switch (req->bmRequestType) {
+      if (init_data_in(transfer, 2)) {
+        switch (transfer->req->bmRequestType) {
           case (REQ_DIR_IN | REQ_TYPE_STANDARD | REQ_REC_DEVICE):
-            *((uint16_t*) control_transfer.data_in) = DEVICE_STATUS_SELF_POWERED;
+            *((uint16_t*) transfer->data_in) = DEVICE_STATUS_SELF_POWERED;
             break;
           case (REQ_DIR_IN | REQ_TYPE_STANDARD | REQ_REC_INTERFACE):
-            *((uint16_t*) control_transfer.data_in) = 0;
+            *((uint16_t*) transfer->data_in) = 0;
             break;
           case (REQ_DIR_IN | REQ_TYPE_STANDARD | REQ_REC_ENDPOINT):
             // TODO Select the requested EP and return halted status
             // Currently no EP supports being halted, so always return 0
-            *((uint16_t*) control_transfer.data_in) = 0;
+            *((uint16_t*) transfer->data_in) = 0;
             break;
           default:
-            cancel_control_transfer(&control_transfer);
+            cancel_control_transfer(transfer);
             break;
         }
       }
       break;
     case SET_ADDRESS:
       // get address from SETUP, handshake, enable new address
-      if (req->bmRequestType == (REQ_DIR_OUT | REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
-        if (req->wIndex == 0 && req->wLength == 0) {
+      if (transfer->req->bmRequestType == (REQ_DIR_OUT | REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
+        if (transfer->req->wIndex == 0 && transfer->req->wLength == 0) {
           // Set new address, but only enable *after* ZLP handshake
-          UDADDR = (uint8_t) (req->wValue & 0x7F);
-          control_transfer.stage = CTRL_HANDSHAKE_OUT;
-          control_transfer.callback_handshake = callback_set_address;
+          UDADDR = (uint8_t) (transfer->req->wValue & 0x7F);
+          transfer->stage = CTRL_HANDSHAKE_OUT;
+          transfer->callback_handshake = callback_set_address;
         }
       }
       break;
     case GET_CONFIGURATION:
-      if (req->bmRequestType == (REQ_DIR_IN| REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
-        if(init_data_in(&control_transfer, 1)) {
-          *((uint8_t*) control_transfer.data_in) = get_configuration_index();
+      if (transfer->req->bmRequestType == (REQ_DIR_IN| REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
+        if(init_data_in(transfer, 1)) {
+          *((uint8_t*) transfer->data_in) = get_configuration_index();
         }
       }
       break;
@@ -197,24 +195,25 @@ static void process_standard_request(const struct UsbSetupPacket_t* req) {
       // if cfg=0 go to address state, otherwise go to configured state with selected config
       // if not addressed yet, consider this command invalid
       // reconfigure after handshake
-      if (valid_configuration_index(req->wValue)) {
-        control_transfer.stage = CTRL_HANDSHAKE_OUT;
-        control_transfer.callback_handshake = callback_set_configuration;
+      if (valid_configuration_index(transfer->req->wValue)) {
+        transfer->stage = CTRL_HANDSHAKE_OUT;
+        transfer->callback_handshake = callback_set_configuration;
       }
       break;
     case GET_DESCRIPTOR:
       // determine which descriptor to transmit and setup transaction
-      if (req->bmRequestType == (REQ_DIR_IN | REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
-        struct descriptor_list_t* head = generate_descriptor_list(req);
+      if (transfer->req->bmRequestType == (REQ_DIR_IN | REQ_TYPE_STANDARD | REQ_REC_DEVICE)) {
+        struct descriptor_list_t* head = generate_descriptor_list(transfer->req);
         struct descriptor_list_t* old_head;
         if (head) {
           // Send until requested size is reached OR all data is sent
           uint16_t list_length = get_list_total_length(head);
-          uint16_t bytes_left = req->wLength < list_length ? req->wLength : list_length;
+          uint16_t requested_length = transfer->req->wLength;
+          uint16_t bytes_left = requested_length < list_length ? requested_length : list_length;
 
-          if (init_data_in(&control_transfer, bytes_left)) {
+          if (init_data_in(transfer, bytes_left)) {
             uint8_t body_length, len;
-            uint8_t* write_ptr = (uint8_t*) control_transfer.data_in;
+            uint8_t* write_ptr = (uint8_t*) transfer->data_in;
             while (head && bytes_left) {
               // Copy header
               len = bytes_left < HEADER_SIZE ? bytes_left : HEADER_SIZE;
@@ -254,39 +253,40 @@ static void process_standard_request(const struct UsbSetupPacket_t* req) {
   }
 }
 
-static void callback_set_address(const struct UsbSetupPacket_t* req) {
+static void callback_set_address(struct control_transfer_t* transfer) {
   // Enable new address
-  const uint8_t address = (uint8_t) (req->wValue & 0x7F);
+  const uint8_t address = (uint8_t) (transfer->req->wValue & 0x7F);
   UDADDR |= _BV(ADDEN);
   if (address) {
     set_device_state(ADDRESSED);
   }
 }
 
-static void callback_set_configuration(const struct UsbSetupPacket_t* req) {
-  set_configuration_index(req->wValue);
-  if (req->wValue == 0) {
+static void callback_set_configuration(struct control_transfer_t* transfer) {
+  set_configuration_index(transfer->req->wValue);
+  if (transfer->req->wValue == 0) {
     set_device_state(DEFAULT);
   }
   else {
     set_device_state(CONFIGURED);
   }
 }
+
 #define VENDOR_REQUEST_PUSH_FRAME 1
 static struct frame_buffer_t* usb_frame;
 static uint8_t* usb_frame_buffer_ptr;
 static void callback_data_usb_frame(struct control_transfer_t* transfer);
 
-static void process_vendor_request(const struct UsbSetupPacket_t* req) {
-  if (req->bmRequestType == (REQ_DIR_OUT | REQ_TYPE_VENDOR | REQ_REC_INTERFACE)) {
+static void process_vendor_request(struct control_transfer_t* transfer) {
+  if (transfer->req->bmRequestType == (REQ_DIR_OUT | REQ_TYPE_VENDOR | REQ_REC_INTERFACE)) {
     // If correct request and request length
-    if (req->bRequest == VENDOR_REQUEST_PUSH_FRAME && req->wLength == FRAME_LENGTH) {
+    if (transfer->req->bRequest == VENDOR_REQUEST_PUSH_FRAME && transfer->req->wLength == FRAME_LENGTH) {
       usb_frame = create_frame();
       if (usb_frame) {
         usb_frame->flags = FRAME_FREE_AFTER_DRAW;
         usb_frame_buffer_ptr = (uint8_t*) usb_frame->buffer;
-        control_transfer.stage = CTRL_DATA_OUT;
-        control_transfer.callback_data_out = callback_data_usb_frame;
+        transfer->stage = CTRL_DATA_OUT;
+        transfer->callback_data_out = callback_data_usb_frame;
       }
     }
   }
@@ -303,19 +303,21 @@ static void callback_data_usb_frame(struct control_transfer_t* transfer) {
   }
 }
 
-static void process_setup(const struct UsbSetupPacket_t* req) {
-  switch (GET_REQUEST_TYPE(req->bmRequestType)) {
+static void process_setup(struct control_transfer_t* transfer) {
+  switch (GET_REQUEST_TYPE(transfer->req->bmRequestType)) {
     case REQ_TYPE_STANDARD:
-      process_standard_request(req);
+      process_standard_request(transfer);
       break;
     case REQ_TYPE_VENDOR:
-      process_vendor_request(req);
+      process_vendor_request(transfer);
       break;
     default:
       break;
   }
 }
 
+static struct control_transfer_t control_transfer;
+static struct UsbSetupPacket_t setup_packet;
 
 ISR(USB_COM_vect) {
   trip_led();
@@ -330,15 +332,13 @@ ISR(USB_COM_vect) {
 
       cancel_control_transfer(&control_transfer);
       control_transfer.callback_handshake = 0;
+      control_transfer.callback_data_out = 0;
       control_transfer.stage = CTRL_SETUP;
 
-      size_t read = fifo_read(&control_transfer.req, sizeof(struct UsbSetupPacket_t));
+      size_t read = fifo_read(&setup_packet, sizeof(struct UsbSetupPacket_t));
       if (read == sizeof(struct UsbSetupPacket_t)) {
-        process_setup(&control_transfer.req);
-      }
-
-      if (control_transfer.stage != CTRL_SETUP) {
-        clear_setup();
+        control_transfer.req = &setup_packet;
+        process_setup(&control_transfer);
       }
 
       if (
@@ -353,12 +353,11 @@ ISR(USB_COM_vect) {
       ) {
         SET_FLAG(UEIENX, RXOUTE);
       }
-
-      // backwards compatiblity STALL
-      if (FLAG_IS_SET(UEINTX, RXSTPI)) {
+      else {
         SET_FLAG(UECONX, STALLRQ);
-        clear_setup();
       }
+
+      clear_setup();
     }
 
     if (FLAG_IS_SET(UEIENX, TXINE) && FLAG_IS_SET(UEINTX, TXINI)) {
@@ -389,7 +388,7 @@ ISR(USB_COM_vect) {
         }
       }
       else if (control_transfer.stage == CTRL_POST_HANDSHAKE) {
-        control_transfer.callback_handshake(&control_transfer.req);
+        control_transfer.callback_handshake(&control_transfer);
         control_transfer.stage = CTRL_IDLE;
         CLEAR_FLAG(UEIENX, TXINE);
       }
@@ -416,7 +415,7 @@ ISR(USB_COM_vect) {
         // Since acknowledging the handshake doesn't require waiting for the host, perform
         // any callback immediately
         if (control_transfer.callback_handshake) {
-          control_transfer.callback_handshake(&control_transfer.req);
+          control_transfer.callback_handshake(&control_transfer);
         }
         control_transfer.stage = CTRL_IDLE;
         CLEAR_FLAG(UEIENX, RXOUTE);
