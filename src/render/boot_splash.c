5ae8bba8912b605258a6b420e5a197fe46535749
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-05-25 18:20:16 +0200

    Make UGent logo renderer more robust.
    Prevent multiple memory allocations if renderer.start is called multiple times,
    and prevent freeing of a null pointer if renderer.stop is called multiple times.

--- src/render/boot_splash.c
+++ src/render/boot_splash.c
@@ -16,8 +16,6 @@ const struct renderer_t* get_boot_splash_renderer() {
   return &SPLASH_RENDERER;
 }
 
-#define LED_OFF (struct led_t) {0, 0, 0, 0}
-#define LED_UGENT_BLUE (struct led_t) {0x08, 10, 30, 96}
 static const uint8_t UGENT_LOGO[LED_COUNT] PROGMEM = {
         0,0,0,0,0,0
      , 0,0,0,0,0,0,0
@@ -34,8 +32,10 @@ static const uint8_t UGENT_LOGO[LED_COUNT] PROGMEM = {
 
 static struct frame_buffer_t* ugent_frame;
 
-void init_splash() {
-  ugent_frame = create_frame();
+void init_splash()  {
+  if (!ugent_frame) {
+    ugent_frame = create_frame();
+  }
   if (ugent_frame) {
     struct led_t* led_ptr = ugent_frame->buffer;
     for (uint8_t led = 0; led < LED_COUNT; ++led) {
@@ -51,8 +51,10 @@ void init_splash() {
 }
 
 void stop_splash() {
-  free(ugent_frame);
-  ugent_frame = 0;
+  if (ugent_frame) {
+    free(ugent_frame);
+    ugent_frame = 0;
+  }
 }
 
 struct frame_buffer_t* render_splash() {
