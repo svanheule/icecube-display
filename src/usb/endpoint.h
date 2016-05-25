4b095ec8636bac29c3e0049c08fd0fc9f4dd3fcf
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-05-25 17:55:45 +0200

    Initial types for refactor to make USB endpoint code more platform independent.

--- src/usb/endpoint.h
+++ src/usb/endpoint.h
@@ -11,6 +11,24 @@ struct ep_hw_config_t {
   uint8_t cfg1;
 };
 
+enum ep_type_t {
+    EP_CONTROL
+  , EP_ISOCHRONOUS_IN
+  , EP_ISOCHRONOUS_OUT
+  , EP_INTERRUPT_IN
+  , EP_INTERRUPT_OUT
+  , EP_BULK_IN
+  , EP_BULK_OUT
+};
+
+struct ep_config_t {
+  uint8_t num;
+  uint16_t size;
+  enum ep_type_t type;
+  // TODO isochronous flags
+  // TODO interval
+};
+
 bool endpoint_configure(const struct ep_hw_config_t* config);
 
 #endif
