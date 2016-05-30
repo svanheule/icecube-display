79611f29d54ae0e5beaf27fe7d897548eec0a124
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-05-30 11:19:22 +0200

    Use array of statically allocated display frames instead of malloc.
    Using a single, continuous array of display frame buffers prevents
    fragmentation of the heap into large unused areas when combined
    with the use of malloc for the USB communication.
    Pre-allocating also gives more control over exhaustion of memory
    and prevents device malfunction (heap/stack collision) by denying
    buffer allocation.

--- src/frame_buffer.c
+++ src/frame_buffer.c
@@ -2,20 +2,58 @@
 #include <stdlib.h>
 #include <util/atomic.h>
 
+// List of statically allocated frame buffers
+#define BUFFER_LIST_LENGTH 3
+static struct frame_buffer_t buffer_list[BUFFER_LIST_LENGTH];
+
+// Mask indicating which buffers are already handed out
+static uint8_t buffer_taken;
+
+static inline bool is_buffer_free(uint8_t buffer) {
+  return !(buffer_taken & (1<<buffer));
+}
+
+static inline void mark_buffer_taken(uint8_t buffer) {
+  buffer_taken |= (1<<buffer);
+}
+
+static inline void mark_buffer_free(uint8_t buffer) {
+  buffer_taken &= ~(1<<buffer);
+}
+
+// Frame memory management functions
 struct frame_buffer_t* create_frame() {
-  struct frame_buffer_t* f;
+  struct frame_buffer_t* f = 0;
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    f = (struct frame_buffer_t*) malloc(sizeof(struct frame_buffer_t));
+    // Look for free available buffer
+    uint8_t buffer = 0;
+    while (buffer < BUFFER_LIST_LENGTH && !is_buffer_free(buffer)) {
+      ++buffer;
+    }
+    // Return free buffer, if any
+    if (buffer != BUFFER_LIST_LENGTH) {
+      mark_buffer_taken(buffer);
+      f = &(buffer_list[buffer]);
+    }
   }
   return f;
 }
 
 void destroy_frame(struct frame_buffer_t* frame) {
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    free(frame);
+    // Compare pointer to list of allocated buffers
+    uint8_t buffer = 0;
+    while (buffer < BUFFER_LIST_LENGTH && frame != &buffer_list[buffer]) {
+      ++buffer;
+    }
+    // If found, mark as free again
+    if (buffer != BUFFER_LIST_LENGTH) {
+      mark_buffer_free(buffer);
+    }
   }
 }
 
+// Commom frame operations
 void clear_frame(struct frame_buffer_t* frame_ptr) {
   if (frame_ptr) {
     struct led_t* write_ptr = frame_ptr->buffer;
