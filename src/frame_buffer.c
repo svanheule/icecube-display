4c3d1af4ff59499dc7229a5d24d9df311a12a017
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-05-30 14:33:02 +0200

    Optimize atomic blocks for frame (de)allocation.

--- src/frame_buffer.c
+++ src/frame_buffer.c
@@ -1,5 +1,4 @@
 #include "frame_buffer.h"
-#include <stdlib.h>
 #include <util/atomic.h>
 
 // List of statically allocated frame buffers
@@ -9,30 +8,24 @@ static struct frame_buffer_t buffer_list[BUFFER_LIST_LENGTH];
 // Mask indicating which buffers are already handed out
 static uint8_t buffer_taken;
 
-static inline bool is_buffer_free(uint8_t buffer) {
-  return !(buffer_taken & (1<<buffer));
-}
-
-static inline void mark_buffer_taken(uint8_t buffer) {
-  buffer_taken |= (1<<buffer);
-}
-
-static inline void mark_buffer_free(uint8_t buffer) {
-  buffer_taken &= ~(1<<buffer);
-}
-
 // Frame memory management functions
 struct frame_buffer_t* create_frame() {
   struct frame_buffer_t* f = 0;
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    // Look for free available buffer
-    uint8_t buffer = 0;
-    while (buffer < BUFFER_LIST_LENGTH && !is_buffer_free(buffer)) {
-      ++buffer;
+    /* Look for free available buffer.
+     * Start by looking at the last buffer and shift the bit mask to the right.
+     * If no free buffer has been encountered by then (i.e. bit set to '0' in buffer_taken)
+     * buffer_mask will become zero and loop evaluation will also end.
+     */
+    uint8_t buffer = BUFFER_LIST_LENGTH-1;
+    uint8_t buffer_mask = 1<<(BUFFER_LIST_LENGTH-1);
+    while (buffer_mask & buffer_taken) {
+      --buffer;
+      buffer_mask >>= 1;
     }
-    // Return free buffer, if any
-    if (buffer != BUFFER_LIST_LENGTH) {
-      mark_buffer_taken(buffer);
+    // Return free buffer, if any. Reuse bit mask from loop to mark buffer as taken.
+    if (buffer_mask) {
+      buffer_taken |= buffer_mask;
       f = &(buffer_list[buffer]);
     }
   }
@@ -42,13 +35,16 @@ struct frame_buffer_t* create_frame() {
 void destroy_frame(struct frame_buffer_t* frame) {
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
     // Compare pointer to list of allocated buffers
-    uint8_t buffer = 0;
-    while (buffer < BUFFER_LIST_LENGTH && frame != &buffer_list[buffer]) {
-      ++buffer;
+    // See create_frame() for details on the variable manipulations.
+    struct frame_buffer_t* buffer = buffer_list + (BUFFER_LIST_LENGTH-1);
+    uint8_t buffer_mask = 1<<(BUFFER_LIST_LENGTH-1);
+    while (frame != buffer && buffer_mask) {
+      --buffer;
+      buffer_mask >>= 1;
     }
     // If found, mark as free again
-    if (buffer != BUFFER_LIST_LENGTH) {
-      mark_buffer_free(buffer);
+    if (buffer_mask) {
+      buffer_taken &= ~buffer_mask;
     }
   }
 }
