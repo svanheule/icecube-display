1edef1350beea5b98889ae333cd301d877f06735
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-10-20 11:27:59 +0200

    Add more resolution to the frame timer for AVR.

--- src/frame_timer.c
+++ src/frame_timer.c
@@ -5,14 +5,13 @@ static void (*callback)();
 
 void init_frame_timer(void (*timer_callback)()) {
   /* Clock is 16MHz
-   * 25 FPS: 640000 counts; prescale 256, compare (2500-1)
-   * 50 FPS: 320000 counts; prescale 256, compare (1250-1)
-   * Prescale factor is 2^(n+2) with n = CS12:CS11:CS10
+   * 25 FPS: 640000 counts; prescale 64, compare (10000-1)
+   * 50 FPS: 320000 counts; prescale 64, compare (5000-1)
    * Set mode to 0100 : CTC with compare to OCR1A
    */
-  OCR1A = (F_CPU/256/25)-1;
+  OCR1A = (F_CPU/64/25)-1;
   // Set compare mode, prescaler, and enable interrupt
-  TCCR1B = (1<<WGM12) | (1<<CS12);
+  TCCR1B = (1<<WGM12) | (3 << CS10);
 
   callback = timer_callback;
   TIMSK1 = (1<<OCIE1A);
