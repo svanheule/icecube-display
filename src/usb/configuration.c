3d643c70c0637e25c183385e0fe6ee116391b979
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-10-24 13:27:06 +0200

    Force configuration info into flash for all platforms.

--- src/usb/configuration.c
+++ src/usb/configuration.c
@@ -1,24 +1,42 @@
 #include "usb/configuration.h"
 #include "usb/endpoint.h"
+#include <string.h>
+#include <avr/pgmspace.h>
 
 struct configuration_t {
   uint8_t endpoint_count;
   const struct ep_config_t* ep_config_list;
 };
 
-static const struct ep_config_t EP_CONFIG_LIST[1] = {
+static const struct ep_config_t CONFIG0_EP_LIST[] PROGMEM = {
   {0, EP_TYPE_CONTROL, EP_DIRECTION_BIDIR, 64}
 };
 
+static const struct ep_config_t CONFIG1_EP_LIST[] PROGMEM = {
+    {0, EP_TYPE_CONTROL, EP_DIRECTION_BIDIR, 64}
+  , {1, EP_TYPE_INTERRUPT, EP_DIRECTION_IN, 4}
+  , {2, EP_TYPE_BULK, EP_DIRECTION_OUT, 64} // TODO determine best size
+};
+
+#define CONFIG(config_list) {sizeof(config_list)/sizeof(config_list[0]), &config_list[0]}
+
 // Configuration list
-static const struct configuration_t DEFAULT_CONFIG = {1, &EP_CONFIG_LIST[0]};
+static const struct configuration_t DEFAULT_CONFIG PROGMEM = CONFIG(CONFIG0_EP_LIST);
+static const struct configuration_t DISPLAY_CONFIG PROGMEM = CONFIG(CONFIG1_EP_LIST);
 
 static bool load_configuration(const struct configuration_t* config) {
+  uint8_t ep_count = pgm_read_byte(&config->endpoint_count);
+  struct ep_config_t* ep_config_list_head;
+  memcpy_P(&ep_config_list_head, &config->ep_config_list, sizeof(ep_config_list_head));
+
   bool config_ok = true;
-  uint8_t endpoint = 0;
-  while (endpoint < config->endpoint_count && config_ok) {
-    config_ok = config_ok && endpoint_configure(&config->ep_config_list[endpoint]);
-    ++endpoint;
+  struct ep_config_t* ep_config_list_end = ep_config_list_head + ep_count;
+
+  while (ep_config_list_head != ep_config_list_end && config_ok) {
+    struct ep_config_t ep_config;
+    memcpy_P(&ep_config, ep_config_list_head, sizeof(ep_config));
+    config_ok = config_ok && endpoint_configure(&ep_config);
+    ep_config_list_head++;
   }
   return config_ok;
 }
