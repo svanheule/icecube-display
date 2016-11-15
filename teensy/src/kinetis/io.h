cda47daded51d44d9f93db4d57550bc8ead219f0
Sander Vanheule <Sander.Vanheule@UGent.be>
2016-11-15 16:01:00 +0100

    Better protection for macro arguments.

--- teensy/src/kinetis/io.h
+++ teensy/src/kinetis/io.h
@@ -7,12 +7,12 @@
 #define _BV(n) (1<<(n))
 
 #define BITBAND_REGISTER_ADDRESS(reg_addr, bit) \
-  ( (volatile uint32_t*) (0x42000000 + ((uint32_t)(reg_addr-0x40000000))*32 + bit*4) )
+  ( (volatile uint32_t*) (0x42000000 + ((uint32_t)((reg_addr)-0x40000000))*32 + (bit)*4) )
 #define ATOMIC_REGISTER_BIT_SET(reg, bit) *BITBAND_REGISTER_ADDRESS(&reg, bit) = 1
 #define ATOMIC_REGISTER_BIT_CLEAR(reg, bit) *BITBAND_REGISTER_ADDRESS(&reg, bit) = 0
 
 #define BITBAND_SRAM_ADDRESS(reg_addr, bit) \
-  ( (volatile uint32_t*) (0x22000000 + ((uint32_t)(reg_addr-0x20000000))*32 + bit*4) )
+  ( (volatile uint32_t*) (0x22000000 + ((uint32_t)((reg_addr)-0x20000000))*32 + (bit)*4) )
 #define ATOMIC_SRAM_BIT_SET(reg, bit) *BITBAND_SRAM_ADDRESS(&reg, bit) = 1
 #define ATOMIC_SRAM_BIT_CLEAR(reg, bit) *BITBAND_SRAM_ADDRESS(&reg, bit) = 0
 
