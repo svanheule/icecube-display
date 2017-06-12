402ec6f354d031bcfaf5456b8c96b30f10289f07
Sander Vanheule <Sander.Vanheule@UGent.be>
2017-06-12 15:29:18 +0200

    Support for string-by-string testing of a display.

--- test_usb/test_draw.py
+++ test_usb/test_draw.py
@@ -18,53 +18,94 @@ from LedDisplay import DisplayManager, DisplayController
 
 manager = DisplayManager()
 
-if len(sys.argv) > 1:
-  offset = int(sys.argv[1])
-else:
-  offset = 0
+import argparse
+parser = argparse.ArgumentParser(description="Test IceCube LED display")
+parser.add_argument(
+        "-m"
+      , "--mode"
+      , choices=["string", "rgb"]
+      , help="Test string per string or display RGB loop"
+      , required=True
+)
+parser.add_argument("-d", "--duration", type=float, help="Duration of a single frame. Defaults to 1/25.", default=1./25)
+parser.add_argument("-c", "--count", type=int, help="Number of frames to render. Defaults to 0.", default=0)
+args = parser.parse_args(sys.argv[1:])
+
+offset = args.count
+duration = args.duration
 
 import colorsys
 
 #
 # Draw an HSV loop to the display
 #
+def draw_hsv(display, offset):
+  pixel_size = display.led_class.DATA_LENGTH
+  rgb_to_data = display.led_class.float_to_led_data
+  buffer_length = display.buffer_length
+  frame = bytearray(buffer_length)
+
+  if display.data_type == DisplayController.DATA_TYPE_IC_STRING:
+    pixel_offset = offset%60
+    for pixel in range(60):
+      hue = (pixel-pixel_offset)/60
+      if hue < 0:
+        hue += 1
+      data = rgb_to_data(colorsys.hls_to_rgb(hue, 0.1, 1.0))
+      for string_offset in range(display.string_count):
+        buffer_offset = string_offset*60+pixel_offset
+        frame[buffer_offset*pixel_size:(buffer_offset+1)*pixel_size] = data
+
+  elif display.data_type == DisplayController.DATA_TYPE_IT_STATION:
+    pixel_offset = offset%display.string_count
+    for pixel in range(display.string_count):
+      hue = (pixel-pixel_offset)/display.string_count
+      if hue < 0:
+        hue += 1
+      data = rgb_to_data(colorsys.hls_to_rgb(hue, 0.1, 1.0))
+      buffer_offset = pixel_size*pixel_offset
+      frame[pixel*pixel_size:(pixel+1)*pixel_size] = data
+
+  return frame
+
+
+def draw_loop(display, offset):
+  "Light up one string (nr. `offset`) of the detector"
+  pixel_size = display.led_class.DATA_LENGTH
+  rgb_to_data = display.led_class.float_to_led_data
+  buffer_length = display.buffer_length
+  frame = bytearray(buffer_length)
+
+  pixel_data = rgb_to_data(0.1, 0.1, 0.1)
+
+  if display.data_type == DisplayController.DATA_TYPE_IC_STRING:
+    string_offset = offset % display.string_count
+    pixel_offset = string_offset*60
+    for pixel in range(60):
+      frame[pixel_offset*pixel_size:(pixel_offset+1)*pixel_size] = pixel_data
+
+  elif display.data_type == DisplayController.DATA_TYPE_IT_STATION:
+    pixel = offset % display.string_count
+    frame[pixel*pixel_size:(pixel+1)*pixel_size] = pixel_data
+
+  return frame
+
 
 if len(manager.displays) and offset:
-  while offset:
+  for offset in range(frame_count):
     logger.debug("{} frames to go".format(offset))
     for i,display in enumerate(manager.displays):
-      pixel_size = display.led_class.DATA_LENGTH
-      rgb_to_data = display.led_class.float_to_led_data
-      buffer_length = display.buffer_length
-      frame = bytearray(buffer_length)
-
-      if display.data_type == DisplayController.DATA_TYPE_IC_STRING:
-        pixel_offset = (offset-1)%60
-        for pixel in range(60):
-          hue = (pixel-pixel_offset)/60
-          if hue < 0:
-            hue += 1
-          data = rgb_to_data(colorsys.hls_to_rgb(hue, 0.1, 1.0))
-          for string_offset in range(display.string_count):
-            buffer_offset = string_offset*60+pixel_offset
-            frame[buffer_offset*pixel_size:(buffer_offset+1)*pixel_size] = data
-
-      elif display.data_type == DisplayController.DATA_TYPE_IT_STATION:
-        pixel_offset = (offset-1)%display.string_count
-        for pixel in range(display.string_count):
-          hue = (pixel-pixel_offset)/display.string_count
-          if hue < 0:
-            hue += 1
-          data = rgb_to_data(colorsys.hls_to_rgb(hue, 0.1, 1.0))
-          buffer_offset = pixel_size*pixel_offset
-          frame[pixel*pixel_size:(pixel+1)*pixel_size] = data
+      if args.mode == 'string':
+        frame = draw_loop(display, offset)
+      elif args.mode == 'rgb':
+        frame = draw_hsv(display, offset)
+      else:
+        frame = bytearray(display.buffer_length)
 
       frame = bytes(frame)
-
       display.transmitDisplayBuffer(frame)
  
-    offset -= 1
-    sleep(1./25)
+    sleep(duration)
 
   for i,display in enumerate(manager.displays):
     logger.debug("Clearing display")
